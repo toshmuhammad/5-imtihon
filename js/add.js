299f9bab94dba466c488dd83905eded3a8221f10
toshmuhammad <toshmuhammad@gmail.com>
2025-04-18 07:39:23 +0500

    nimadir

--- js/add.js
+++ js/add.js
@@ -1,17 +1,13 @@
 const form = document.querySelector('#formLogin');
-
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-
   const name = document.getElementById('name').value.trim();
   const price = document.getElementById('price').value.trim();
   const description = document.getElementById('desc').value.trim();
-
   if (!name || !price || !description) {
-    alert("Iltimos, barcha maydonlarni to‘ldiring!");
+    alert("hamma joyni toldiring");
     return;
   }
-
   let cars = JSON.parse(localStorage.getItem('cars')) || [];
   cars.unshift({ name, price, description });
   localStorage.setItem('cars', JSON.stringify(cars));
